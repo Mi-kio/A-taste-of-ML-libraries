0d306a5ffe0325ba64d5f2851f24d67f9e47e2ce
SAKSHI JAIN <93917702+Mi-kio@users.noreply.github.com>
2022-02-27 23:54:40 +0530

    trains and runs deep neural n/w

---
+++ Tensorflow.py
@@ -0,0 +1,21 @@
+# Python program using TensorFlow
+# for multiplying two arrays
+
+# import `tensorflow`
+import tensorflow as tf
+
+# Initialize two constants
+x1 = tf.constant([1, 2, 3, 4])
+x2 = tf.constant([5, 6, 7, 8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+# Initialize the Session
+sess = tf.Session()
+
+# Print the result
+print(sess.run(result))
+
+# Close the session
+sess.close()
