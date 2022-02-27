cf0f84bc1f5e4a83bc310093448985c50a5ca52a
SAKSHI JAIN <93917702+Mi-kio@users.noreply.github.com>
2022-02-27 23:51:17 +0530

    Added scipy.py

---
+++ scipy.py
@@ -0,0 +1,20 @@
+# Python script using Scipy
+# for image manipulation
+
+from scipy.misc import imread, imsave, imresize
+
+# Read a JPEG image into a numpy array
+img = imread('D:/Programs / cat.jpg') # path of the image
+print(img.dtype, img.shape)
+
+# Tinting the image
+img_tint = img * [1, 0.45, 0.3]
+
+# Saving the tinted image
+imsave('D:/Programs / cat_tinted.jpg', img_tint)
+
+# Resizing the tinted image to be 300 x 300 pixels
+img_tint_resize = imresize(img_tint, (300, 300))
+
+# Saving the resized tinted image
+imsave('D:/Programs / cat_tinted_resized.jpg', img_tint_resize)
